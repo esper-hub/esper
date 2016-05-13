199cf5207cc8b32824dd67f56e334e952d2083ea
Florian Klink <flokli@flokli.de>
2016-05-14 00:26:20 +0200

    format include/user_config.h

--- include/user_config.h
+++ include/user_config.h
@@ -5,38 +5,39 @@
 extern "C" {
 #endif
 
-	// UART config
-	#define SERIAL_BAUD_RATE 115200
+// UART config
+#define SERIAL_BAUD_RATE 115200
 
-	// ESP SDK config
-	#define LWIP_OPEN_SRC
-	#define USE_US_TIMER
+// ESP SDK config
+#define LWIP_OPEN_SRC
+#define USE_US_TIMER
 
-	// Default types
-	#define __CORRECT_ISO_CPP_STDLIB_H_PROTO
-	#include <limits.h>
-	#include <stdint.h>
+// Default types
+#define __CORRECT_ISO_CPP_STDLIB_H_PROTO
 
-	// Override c_types.h include and remove buggy espconn
-	#define _C_TYPES_H_
-	#define _NO_ESPCON_
+#include <limits.h>
+#include <stdint.h>
 
-	// Updated, compatible version of c_types.h
-	// Just removed types declared in <stdint.h>
-	#include <espinc/c_types_compatible.h>
+// Override c_types.h include and remove buggy espconn
+#define _C_TYPES_H_
+#define _NO_ESPCON_
 
-	// System API declarations
-	#include <esp_systemapi.h>
+// Updated, compatible version of c_types.h
+// Just removed types declared in <stdint.h>
+#include <espinc/c_types_compatible.h>
 
-	// C++ Support
-	#include <esp_cplusplus.h>
-	// Extended string conversion for compatibility
-	#include <stringconversion.h>
-	// Network base API
-	#include <espinc/lwip_includes.h>
+// System API declarations
+#include <esp_systemapi.h>
 
-	// Beta boards
-	#define BOARD_ESP01
+// C++ Support
+#include <esp_cplusplus.h>
+// Extended string conversion for compatibility
+#include <stringconversion.h>
+// Network base API
+#include <espinc/lwip_includes.h>
+
+// Beta boards
+#define BOARD_ESP01
 
 #ifdef __cplusplus
 }
