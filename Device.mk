38bc639485ccb96e2b3bd89147a39ca6e8b9e88b
Dustin Frisch <fooker@lab.sh>
2016-11-24 23:27:42 +0100

    Make infrastructure for multi-device support

---
+++ Device.mk
@@ -0,0 +1,62 @@
+
+# Use load configuration
+ifndef CONFIG
+$(info $(BASEDIR)/Configuration.mk)
+include $(BASEDIR)/Configuration.mk
+else
+include $(BASEDIR)/Configuration.mk.$(CONFIG)
+endif
+
+
+# Sanity checking some required variables
+ifndef SMING_HOME
+$(error SMING_HOME is not set. Please configure it in Configuration.mk or set in environment)
+endif
+
+ifndef ESP_HOME
+$(error ESP_HOME is not set. Please configure it in Configuration.mk or set in environment)
+endif
+
+
+MODULES += $(BASEDIR)/framework/devices
+MODULES += $(BASEDIR)/framework/features
+MODULES += $(BASEDIR)/framework/managers
+MODULES += $(BASEDIR)/framework/util
+MODULES += $(BASEDIR)/framework
+
+MODULES += $(BASEDIR)/devices/$(DEVICE)
+
+
+# Ensure we have a version string
+VERSION ?= "SNAPSHOT"
+
+
+# Pass options to source code
+USER_CFLAGS += -DMQTT_HOST=\"$(MQTT_HOST)\"
+USER_CFLAGS += -DMQTT_PORT=$(MQTT_PORT)
+USER_CFLAGS += -DMQTT_REALM=\"$(MQTT_REALM)\"
+
+ifdef HEARTBEAT_TOPIC
+USER_CFLAGS += -DHEARTBEAT_TOPIC=\"$(HEARTBEAT_TOPIC)\"
+endif
+
+USER_CFLAGS += -DVERSION=\"$(VERSION)\"
+
+USER_CFLAGS += -O3
+
+
+# Disable SPIFFS
+DISABLE_SPIFFS = 1
+
+
+# Use alternative bootloader
+RBOOT_ENABLED = 1
+RBOOT_BIG_FLASH = 0
+
+RBOOT_LD_0 = $(BASEDIR)/rom0.ld
+RBOOT_LD_1 = $(BASEDIR)/rom1.ld
+
+
+# Include main makefile
+include $(SMING_HOME)/Makefile-rboot.mk
+
--- Makefile
+++ Makefile
@@ -1,44 +1,21 @@
-ifndef CONFIG
-$(info CONFIG is not defined - using default)
-include ./Configuration.mk
-else
-$(info Using Configuration.mk.$(CONFIG))
-include ./Configuration.mk.$(CONFIG)
-endif
+export BASEDIR := $(CURDIR)
 
-ifndef SMING_HOME
-$(error SMING_HOME is not set. Please configure it in Configuration.mk or set in environment)
-endif
+BUILDDIR = $(BASEDIR)/build
 
-ifndef ESP_HOME
-$(error ESP_HOME is not set. Please configure it in Configuration.mk or set in environment)
-endif
+# Find devices
+DEVICES := $(sort $(notdir $(wildcard $(CURDIR)/devices/*)))
 
-MODULES += framework/devices
-MODULES += framework/features
-MODULES += framework/managers
-MODULES += framework/util
-MODULES += framework
 
-MODULES += devices/screen
+all: $(DEVICES)
 
-VERSION ?= "SNAPSHOT"
 
-USER_CFLAGS += -DMQTT_HOST=\"$(MQTT_HOST)\"
-USER_CFLAGS += -DMQTT_PORT=$(MQTT_PORT)
-USER_CFLAGS += -DMQTT_REALM=\"$(MQTT_REALM)\"
+$(DEVICES):
+	mkdir -pv $(BUILDDIR)/$@
+	$(MAKE) -C $(BUILDDIR)/$@ -f $(CURDIR)/Device.mk DEVICE=$@
 
-ifdef HEARTBEAT_TOPIC
-USER_CFLAGS += -DHEARTBEAT_TOPIC=\"$(HEARTBEAT_TOPIC)\"
-endif
 
-USER_CFLAGS += -DVERSION=\"$(VERSION)\"
+clean:
+	rm -f -r $(BUILDDIR)
 
-USER_CFLAGS += -O3
+.PHONY: all $(DEVICES) clean
 
-DISABLE_SPIFFS = 1
-
-RBOOT_ENABLED = 1
-RBOOT_BIG_FLASH = 0
-
-include $(SMING_HOME)/Makefile-rboot.mk
