43ad24006943ea8f3a337e1f01543b18dbff4b6d
Dustin Frisch <fooker@lab.sh>
2017-01-07 00:04:21 +0100

    Added button and LED to sonoff device

--- devices/sonoff/Impl.cpp
+++ devices/sonoff/Impl.cpp
@@ -1,20 +1,34 @@
 #include "../features/Socket.h"
+#include "../features/Button.h"
+#include "../features/Light.h"
 #include "Device.h"
 
 
 constexpr const char SOCKET_NAME[] = "socket";
 constexpr const uint16_t SOCKET_GPIO = 12;
 
+constexpr const char BUTTON_NAME[] = "button";
+constexpr const uint16_t BUTTON_GPIO = 0;
+
+constexpr const char LIGHT_NAME[] = "light";
+constexpr const uint16_t LIGHT_GPIO = 13;
+
 
 class SonoffDevice : public Device {
 public:
     SonoffDevice() :
-            socket(this) {
+            socket(this),
+            button(this),
+            light(this) {
         this->add(&(this->socket));
+        this->add(&(this->button));
+        this->add(&(this->light));
     }
 
 private:
     OnOffFeature<SOCKET_NAME, SOCKET_GPIO, false, 1> socket;
+    Button<BUTTON_NAME, BUTTON_GPIO, true> button;
+    Light<LIGHT_NAME, LIGHT_GPIO> light;
 };
 
 
